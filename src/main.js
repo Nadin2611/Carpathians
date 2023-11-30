ea0115ed7e8d1c272b8c16829e6485b8868bbbab
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-11-30 23:23:16 +0200

    reviews

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './css/styles.css';
 
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
