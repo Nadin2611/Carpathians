fedf970cc851f2ca093c94d478f1f1820fb1f826
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-12-12 21:49:24 +0200

    modal

--- src/js/modal-choice.js
+++ src/js/modal-choice.js
@@ -1,33 +1,62 @@
-const modalEl = document.querySelector('.modal-choice');
+const moreDetailsButtons = document.querySelectorAll('.tours-item-button');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
 let isModalOpen = false;
 
-export function openModal() {
-  console.log('Opening modal');
-  if (!isModalOpen) {
-    modalEl.classList.remove('is-hidden');
-    console.log(modalEl);
-    console.log(isModalOpen);
+const timeout = 800;
 
-    isModalOpen = true;
+if (moreDetailsButtons.length > 0) {
+  for (let index = 0; index < moreDetailsButtons.length; index++) {
+    const button = moreDetailsButtons[index];
+    button.addEventListener('click', openModal);
   }
 }
 
-export function closeModal() {
-  if (isModalOpen) {
-    modalEl.classList.add('is-hidden');
-    isModalOpen = false;
+const modalCloseButtons = document.querySelectorAll('.more-details-close-btn');
+if (modalCloseButtons.length > 0) {
+  for (let index = 0; index < modalCloseButtons.length; index++) {
+    const element = modalCloseButtons[index];
+    element.addEventListener('click', closeModal);
   }
 }
 
-// Присвоєння подій на кнопки відкриття модального вікна
-const moreDetailsButtons = document.querySelectorAll('.tours-item-button');
-moreDetailsButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    console.log(moreDetailsButtons);
-    openModal();
-    console.log(isModalOpen);
-  });
-});
+// const modalEl = document.querySelector('.backdrop');
+// const moreDetailsButton = document.querySelector('.tours-item-button');
+// let isModalOpen = false;
+
+// export function openModal() {
+//   console.log('Opening modal');
+//   if (!isModalOpen) {
+//     modalEl.classList.remove('is-hidden');
+
+//     console.log(moreDetailsButton);
+//     isModalOpen = true;
+//     console.log(isModalOpen);
+//   }
+// }
+
+// export function closeModal() {
+//   if (isModalOpen) {
+//     modalEl.classList.add('is-hidden');
+//     isModalOpen = false;
+//   }
+// }
+
+// moreDetailsButton.addEventListener('click', openModal);
+
+// const moreDetailsButton = document.querySelector('.tours-item-button');
+// moreDetailsButton.addEventListener('click', openModal());
+
+// // Присвоєння подій на кнопки відкриття модального вікна
+// const moreDetailsButtons = document.querySelectorAll('.tours-item-button');
+// moreDetailsButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     console.log(moreDetailsButtons);
+//     openModal();
+//     console.log(isModalOpen);
+//   });
+// });
 
 // moreDetailsButtons.forEach(button => {
 //   button.addEventListener('click', () => {
